83c26b882e31196b7f4d92359009f7039daeab6c
varsha-302007 <alwaysfocused2007@gmail.com>
2026-05-23 10:00:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import google.generativeai as genai
+
+# Configure API
+genai.configure(api_key="AIzaSyCceaokcREd-85ClX8AaWe2_UFYIAHrv2s")
+
+# Load model
+model = genai.GenerativeModel("gemini-flash-latest")
+
+# Streamlit UI
+st.title("AI Quiz Generator")
+
+topic = st.text_input("Enter Topic")
+
+if st.button("Generate Quiz"):
+
+    if topic:
+
+        prompt = f"Create 5 multiple choice questions about {topic}"
+
+        response = model.generate_content(prompt)
+
+        st.write(response.text)
+
+    else:
+        st.warning("Please enter a topic")
